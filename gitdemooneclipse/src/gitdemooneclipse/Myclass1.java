a15d7dfb8689382418f622569d5188b0b7b1a78f
pranali.nalawade <pranali.nalawade@gmail.com>
2021-05-24 18:13:39 +0530

    Committing second time adding myclass1.java

---
+++ gitdemooneclipse/src/gitdemooneclipse/Myclass1.java
@@ -0,0 +1,11 @@
+package gitdemooneclipse;
+
+public class Myclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good evening");
+
+	}
+
+}
